da5ea9fcc2b1547ce01545a0ea974bfde3851435
PygmalesDev <uk084094@student.uni-kassel.de>
2024-10-07 23:18:22 +0200

    (feat): added nested formatting and url tokenization

--- src/unit_tests/test_split_text.py
+++ src/unit_tests/test_split_text.py
@@ -1,18 +1,38 @@
 import unittest
 
-from src.utils.functools import split_markdown_text
+from src.utils.functools import split_markdown
 from src.utils.constants import TextType
 from src.nodes.textnode import TextNode
 
 class TestSplitText(unittest.TestCase):
     def test_split_raw(self):
-        result = split_markdown_text("Hello, world!")
-        self.assertEqual(1, len(result))
-        self.assertEqual(result[0], TextNode("Hello, world!", TextType.TEXT))
+        result = split_markdown("Hello, world!")
+        print(result.to_html())
 
     def test_split_typed(self):
-        result = split_markdown_text("This text contains **bold**, *italic* and `code` formatting types, as well as plain text")
+        result = split_markdown("This text contains **bold**, *italic* and `code` formatting types, as well as plain text")
+        print(result.to_html())
+    
+    def test_split_multiple_paragraphs(self):
+        result = split_markdown(
+        """This text is split in multiple lines. **This is the first paragraph and its bold**. But not entirely!!\n\nThis is the second paragraph, and its written in *italic*. How cool is that!\n\nThis is the third line and it contains some `code lines`. Awesome""")
+        print(result.to_html())
+    
+    def test_nested_formatting(self):
+        result = split_markdown("This text contains **some nasty *nested* formatting `thingies`. I really hate them *but there's no other way*. Ehe**")
+        print(result.to_html())
+    
+    def test_formatted_paragraps(self):
+        result = split_markdown("""**Here are some formatted paragraphs**\n\n*Sometimes there's only one way to make it right*\n\n*I've chosen this **path** myself...*""")
+        print(result.to_html())
 
+    def test_format_empty(self):
+        result = split_markdown("")
+        print(result.to_html())
+    
+    def test_extract_links(self):
+        result = split_markdown("This text **contains** ![image](https://imgs.org) - an image, and a link to a website: [link to a website](https://website.org)! So awesome")
+        print(result.to_html())
 
 if __name__ == '__main__':
     unittest.main()
--- src/utils/functools.py
+++ src/utils/functools.py
@@ -1,54 +1,84 @@
-from src.utils.constants import TextType
-from src.nodes.textnode import TextNode
 from src.nodes.leafnode import LeafNode
+from src.nodes.parentnode import ParentNode
 
-def split_markdown_text(text: str) -> list[TextNode]:
-    paragraphs = text.split('\n')
+def split_markdown(text: str) -> ParentNode:
+    parents = list(map(lambda parag: tokenize_paragraph(parag), text.split("\n\n")))
+    return ParentNode(children=parents) if len(parents) > 1 else parents[0]
+
+def tokenize_paragraph(parag: str, parentag: str = 'p') -> ParentNode:
     nodes = []
+
+    st_ind = 0 
+    indxl = 0
+
+    def move_index(mov: int):
+        nonlocal st_ind, indxl
+
+        indxl = mov
+        st_ind = indxl+1
+
+    def substr(start: int = None, end: int = None):
+        nonlocal parag
+
+        return (parag[start:] if end is None 
+                else parag[:end] if start is None 
+                else parag[start:end])
+
+    def lit(index: int = 0) -> str:
+        nonlocal parag
+
+        return parag[index]
     
-    for parag in paragraphs:
-        st_ind = 0 
-        indxl = 0
-        while indxl < len(parag)-1:
-            lit = parag[indxl]
+    # Iterate the text body from the first letter to the last. 
+    # Split text parts on raw and formatted upon finding key symbols.
+    while indxl < len(parag)-1:
+        lit = parag[indxl]
 
-            if lit in ['*', '`']: 
-                indxtxt = 1 if parag[indxl+1] == lit else 0 
-                indxn = indxl + indxtxt + parag[indxl+indxtxt+1:].find(lit) + 1
-                
-                if indxn < indxl: raise Exception("Caught an error while tokenizing input string after index " + indxl)
-                
-                txtype = TextType.CODE
-                if lit == '*':
-                    if indxtxt > 0: txtype = TextType.BOLD
-                    else: txtype = TextType.ITALIC
+        match lit:
+            case '!' | '[':     
+                link_node, indxn = extract_link(substr(indxl))
+                if link_node is not None:
+                    nodes.append(get_raw_leaf(parag, st_ind, indxl))
+                    nodes.append(link_node)
+                    move_index(indxl+indxn)
 
-                nodes.append(TextNode(text=parag[st_ind:indxl], text_type=TextType.TEXT))
-                nodes.append(TextNode(text=parag[indxl+indxtxt+1:indxn], text_type=txtype))
+            case '*' | '`':
+                if False:
+                    pass
+                else:
+                    indxtxt = 1 if parag[indxl+1] == lit else 0 
+                    indxn = indxl + indxtxt + substr(indxl+indxtxt+1).find(lit+lit*indxtxt) + 1
+                    
+                    txtag = 'code'
+                    if lit == '*':
+                        if indxtxt > 0: txtag = 'b'
+                        else: txtag = 'i'
+                        
+                    nodes.append(get_raw_leaf(parag, st_ind, indxl))
 
-                indxl = indxn+indxtxt+1
-                st_ind = indxl
-            else:
-                indxl +=1
+                    body = substr(indxl+indxtxt+1, indxn)
+                    if '*' in body or '`' in body: nodes.append(tokenize_paragraph(body, txtag))
+                    else: nodes.append(LeafNode(value=body, tag=txtag))
+
+                    move_index(indxn+indxtxt)
             
-        if st_ind != indxl:
-            nodes.append(TextNode(text=parag[st_ind:], text_type=TextType.TEXT))
+        indxl +=1
 
-    return nodes
+    if st_ind != indxl: nodes.append(get_raw_leaf(parag, st_ind))
+    
+    return ParentNode(tag=parentag, children=nodes)
 
-def textnode_to_htmlnode(node: TextNode) -> LeafNode:
-    match node.text_type:
-        case TextType.TEXT:
-            return LeafNode(text=node.text)
-        case TextType.BOLD:
-            return LeafNode(text=node.text, tag="b")
-        case TextType.ITALIC:
-            return LeafNode(text=node.text, tag="i")
-        case TextType.CODE:
-            return LeafNode(text=node.text, tag="code")
-        case TextType.LINK:
-            return LeafNode(text=node.text, tag="a", props={"href":node.url})
-        case TextType.IMAGE:
-            return LeafNode(text="", tag="img", props={"src":node.url, "alt":node.text})
-        case _:
-            raise Exception("Tag parameter of the TextNode instance doesn't exist")
+
+def extract_link(text: str) -> (str, int):
+    if all((let in text for let in ['[', ']', '(', ')'])):
+        end = text.index(')')
+
+        val = text[text.index('[')+1:text.index(']')]
+        url = text[text.index('(')+1:end]
+        tg = 'img' if '!' in text[:end] else 'a'
+        return LeafNode(tag=tg, value=val, props={"url": url}), end
+    return None, None
+
+def get_raw_leaf(body:str, st_indx: int, end_indx: int = 0) -> LeafNode:
+        return (LeafNode(value=body[st_indx:end_indx]) if end_indx != 0 
+                else LeafNode(value=body[st_indx:]))
